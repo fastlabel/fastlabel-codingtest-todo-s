9045886aaa560d520ee1142270677b802e69ba7c
loasnir <33513916+loasnir@users.noreply.github.com>
2024-08-06 23:25:41 +0900

    :recycle: Standardize writing style throughout

--- api/src/repositories/item-repository.ts
+++ api/src/repositories/item-repository.ts
@@ -5,36 +5,32 @@ import { Item } from "../entities/item";
 @EntityRepository(Item)
 export class ItemRepository extends Repository<Item> {
   async findByIdOrFail(id: string): Promise<ItemDto> {
-    const query = this.createQueryBuilder("items").where("items.id = :id", {
-      id,
-    });
-    return ItemDto.fromEntity(await query.getOneOrFail());
+    const query = this.createQueryBuilder("items")
+      .where("items.id = :id", { id });
+    const entity = await query.getOneOrFail();
+    return ItemDto.fromEntity(entity);
   }
 
   async findLastByOrder(): Promise<ItemDto | undefined> {
-    const entity = await this.createQueryBuilder("items")
-      .orderBy("items.order", "DESC")
-      .getOne();
+    const query = this.createQueryBuilder("items")
+      .orderBy("items.order", "DESC");
+    const entity = await query.getOne();
     if (!entity) return undefined;
     return ItemDto.fromEntity(entity);
   }
 
   async get(): Promise<ItemDto[]> {
-    let entities = await this.createQueryBuilder("items")
+    const query = this.createQueryBuilder("items")
       .orderBy("items.order", "ASC")
-      .getMany();
-    const results = [] as any;
-    entities.forEach((e) => {
-      results.push(ItemDto.fromEntity(e));
-    });
-    return results;
+    const entities = await query.getMany();
+    return entities.map((entity) => ItemDto.fromEntity(entity));
   }
 
   async search(keyword: string): Promise<ItemDto[]> {
-    const results = await this.createQueryBuilder("items")
+    const query = this.createQueryBuilder("items")
       .where("items.content LIKE :keyword", { keyword: `%${keyword}%` })
       .orderBy("items.order", "ASC")
-      .getMany();
-    return results.map((e) => ItemDto.fromEntity(e));
+    const entities = await query.getMany();
+    return entities.map((entity) => ItemDto.fromEntity(entity));
   }
 }
